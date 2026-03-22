54d4fa300358913f8ddde30cb805027560267f71
Shinya <aitech.guru365@gmail.com>
2026-03-22 16:46:04 -0400

    Replace JSON with SQLite database

--- app/streamlit_app.py
+++ app/streamlit_app.py
@@ -1,3 +1,8 @@
+import sys
+import os
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 import streamlit as st
 from src.task_manager import add_task, list_tasks, delete_task, edit_task
 
