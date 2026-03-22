e2c9b19925ecc5c62e4cbea226190e7c16383497
Shinya <aitech.guru365@gmail.com>
2026-03-22 13:41:41 -0400

    Update main menu

--- src/main.py
+++ src/main.py
@@ -3,7 +3,9 @@ from src.task_manager import add_task, list_tasks
 while True:
     print("\n1. Add Task")
     print("2. List Tasks")
-    print("3. Exit")
+    print("3. Delete Task")
+    print("4. Edit Task")
+    print("5. Exit")
 
     choice = input("Choose: ")
 
@@ -15,4 +17,15 @@ while True:
         list_tasks()
     
     elif choice == "3":
+        list_tasks()
+        index = int(input("Enter task number to delete: ")) - 1
+        delete_task(index)
+    
+    elif choice == "4":
+        list_tasks()
+        index = int(input("Enter task number to edit: ")) - 1
+        new_task = input("Enter new task: ")
+        edit_task(index, new_task)
+        
+    elif choice == "5":
         break
